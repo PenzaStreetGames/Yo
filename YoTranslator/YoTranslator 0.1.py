0146c8e3b84e7df093b88202b6e7d9f8dd80719d
PenzaStreetGames <parassitn@mail.ru>
2018-12-24 21:38:47 +0400

    24.12.2018

--- YoTranslator/YoTranslator 0.1.py
+++ YoTranslator/YoTranslator 0.1.py
@@ -1,12 +1,22 @@
 key_words = ["while", "if", "print"]
 signs = ["=", "[", "]", "<", ">", ":", "+", "(", ")", ","]
 space, empty = " ", ""
+special_symbols = {"command_end": "*ce",
+                   "indent": "*i"}
+
+tokens = []
+pre_symbol = ""
+word = ""
+
+commands = []
+pre_indent = 0
+
+class TokenError(Exception):
+    pass
 
 
 def token_split(text):
-    tokens = []
-    pre_symbol = ""
-    word = ""
+    global tokens, pre_symbol, word
 
     def add_word():
         global word, tokens
@@ -15,6 +25,49 @@ def token_split(text):
         word = ""
 
     for symbol in text:
-        if symbol == space:
-            if pre_symbol != space:
+        if symbol == "\n" or symbol == "\r":
+            add_word()
+            pre_symbol = "sign"
+            word = special_symbols["command_end"]
+        elif symbol == space:
+            if word == special_symbols["command_end"]:
                 add_word()
+                word = special_symbols["indent"]
+                pre_symbol = "sign"
+            elif word == special_symbols["indent"]:
+                add_word()
+                word = special_symbols["indent"]
+                pre_symbol = "sign"
+            elif pre_symbol != space:
+                add_word()
+                pre_symbol = "space"
+            else:
+                pass
+        elif symbol in signs:
+            add_word()
+            pre_symbol = "sign"
+            word += symbol
+        elif symbol.isalpha():
+            if pre_symbol == "sign":
+                add_word()
+            pre_symbol = "alpha"
+            word += symbol
+        elif symbol.isdigit():
+            if pre_symbol == "sign":
+                add_word()
+            pre_symbol = "digit"
+            word += symbol
+        else:
+            raise TokenError(f"Неизвестный символ {symbol}")
+    add_word()
+    return tokens
+
+
+def command_split():
+    pass
+
+
+if __name__ == '__main__':
+    with open(f"{input()}.yotext", "r", encoding="utf-8") as infile:
+        text = token_split(infile.read())
+        print(text)
