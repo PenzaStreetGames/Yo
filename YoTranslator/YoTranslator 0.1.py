8cacc150dd56cdd0ae02fb5b142c57ecada342b5
Gamba232 <parassitn@mail.ru>
2018-12-25 14:58:26 +0300

    25.12.2018

--- YoTranslator/YoTranslator 0.1.py
+++ YoTranslator/YoTranslator 0.1.py
@@ -2,7 +2,8 @@ key_words = ["while", "if", "print"]
 signs = ["=", "[", "]", "<", ">", ":", "+", "(", ")", ","]
 space, empty = " ", ""
 special_symbols = {"command_end": "*ce",
-                   "indent": "*i"}
+                   "indent": "*i",
+                   "line_indent": "*i{}"}
 
 tokens = []
 pre_symbol = ""
@@ -10,6 +11,8 @@ word = ""
 
 commands = []
 pre_indent = 0
+command = []
+pre_token = ""
 
 class TokenError(Exception):
     pass
@@ -26,9 +29,10 @@ def token_split(text):
 
     for symbol in text:
         if symbol == "\n" or symbol == "\r":
-            add_word()
-            pre_symbol = "sign"
-            word = special_symbols["command_end"]
+            if word != special_symbols["command_end"]:
+                add_word()
+                pre_symbol = "sign"
+                word = special_symbols["command_end"]
         elif symbol == space:
             if word == special_symbols["command_end"]:
                 add_word()
@@ -63,11 +67,43 @@ def token_split(text):
     return tokens
 
 
-def command_split():
-    pass
+def command_split(tokens):
+    global commands, command, pre_token, pre_indent
+
+    def add_command():
+        global command, commands
+        if command:
+            commands += [command]
+        command = []
+
+    for token in tokens:
+        if token == special_symbols["indent"]:
+            if pre_token == special_symbols["indent"]:
+                pre_indent += 1
+            elif pre_token == special_symbols["command_end"]:
+                pre_indent = 0
+                pre_indent += 1
+            pre_token = special_symbols["indent"]
+        elif token == special_symbols["command_end"]:
+            add_command()
+            pre_token = special_symbols["command_end"]
+            pre_indent = 0
+        else:
+            if pre_token == special_symbols["indent"]:
+                command += [special_symbols["line_indent"].format(pre_indent)]
+            elif pre_token == special_symbols["command_end"]:
+                command += [special_symbols["line_indent"].format(pre_indent)]
+            elif pre_token == empty:
+                command += [special_symbols["line_indent"].format(pre_indent)]
+            command += [token]
+            pre_token = token
+    add_command()
+    return commands
 
 
 if __name__ == '__main__':
     with open(f"{input()}.yotext", "r", encoding="utf-8") as infile:
-        text = token_split(infile.read())
-        print(text)
+        stage_1 = token_split(infile.read())
+        print(stage_1)
+        stage_2 = command_split(stage_1)
+        print(stage_2)
