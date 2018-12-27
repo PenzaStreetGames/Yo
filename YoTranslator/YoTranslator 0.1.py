58f988471d202b5a90606ca459416b24f4860bff
Gamba232 <parassitn@mail.ru>
2018-12-27 23:53:10 +0300

    27.12.2018

--- YoTranslator/YoTranslator 0.1.py
+++ YoTranslator/YoTranslator 0.1.py
@@ -1,21 +1,59 @@
-key_words = ["while", "if", "print"]
+key_words = ["while", "if"]
+functions = ["print"]
 signs = ["=", "[", "]", "<", ">", ":", "+", "(", ")", ","]
 space, empty = " ", ""
 special_symbols = {"command_end": "*ce",
                    "indent": "*i",
                    "line_indent": "*i{}"}
 
+# for token_split
 tokens = []
 pre_symbol = ""
 word = ""
 
+# for command_split
 commands = []
 pre_indent = 0
 command = []
 pre_token = ""
 
+# for token_analise
+pre_group = ""
+
+group_priority = {
+    "object": 1,
+    "brackets": 2,
+    "sub_object": 3,
+    "call": 4,
+    "math": 5,
+    "comparison": 6,
+    "logic": 7,
+    "equating": 8,
+    "structure": 9,
+    "key_word": 10
+}
+
+priority = {
+
+}
+
 class TokenError(Exception):
     pass
+
+
+class Token:
+    def __init__(self, name, group):
+        self.name = name
+        self.group = group
+
+    def token_analyse(self):
+        pass
+
+    def __str__(self):
+        return f"{self.group} \"{self.name}\""
+
+    def __repr__(self):
+        return f"{self.group} \"{self.name}\""
 
 
 def token_split(text):
@@ -100,6 +138,58 @@ def command_split(tokens):
     add_command()
     return commands
 
+def token_analise(out_commands):
+    global commands
+
+    result = []
+    for command in commands:
+        result += [[]]
+        for token in command:
+            group = ""
+            if token.startswith(special_symbols["indent"]):
+                pass
+            elif token.isdigit():
+                group = "object"
+            elif token in signs:
+                if token == "+":
+                    group = "math"
+                elif token == "=":
+                    group = "equating"
+                elif token == ">":
+                    group = "comparison"
+                elif token == "<":
+                    group = "comparison"
+                elif token == ":":
+                    group = "structure"
+                elif token == "[":
+                    if pre_group != "object":
+                        group = "object"
+                    else:
+                        group = "sub_object"
+                elif token == "]":
+                    group = "sub_object"
+                elif token == "(":
+                    if pre_group != "object":
+                        group = "brackets"
+                    else:
+                        group = "call"
+                elif token == ")":
+                    group = "call"
+                elif token == ",":
+                    group = "brackets"
+            elif token in key_words:
+                group = "key_word"
+            elif token[0].isalpha():
+                group = "object"
+            else:
+                raise TokenError(f"Неизвестный токен {token}")
+            token = Token(token, group)
+            result[-1] += [token]
+            pre_group = group
+    commands = result.copy()
+
+    return result
+
 
 def command_analise():
     pass
@@ -115,3 +205,5 @@ if __name__ == '__main__':
     print(stage_1)
     stage_2 = command_split(stage_1)
     print(stage_2)
+    stage_3 = token_analise(stage_2)
+    print(stage_3)
