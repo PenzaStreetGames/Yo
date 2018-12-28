37dad147127ac77704882735435f3156ff6d2c1b
Gamba232 <parassitn@mail.ru>
2018-12-28 13:37:27 +0300

    28.12.2018

--- YoTranslator/YoTranslator 0.1.py
+++ YoTranslator/YoTranslator 0.1.py
@@ -30,30 +30,103 @@ group_priority = {
     "logic": 7,
     "equating": 8,
     "structure": 9,
-    "key_word": 10
+    "key_word": 10,
+    "": 100
 }
 
 priority = {
-
+    "object":
+    {
+        "name": 1,
+        "number": 1,
+        "list": 1
+    },
+    "brackets":
+    {
+        "(": 1,
+        ")": 1,
+        ",": 1
+    },
+    "sub_object":
+    {
+        ".": 1,
+        "[": 1,
+        "]": 1
+    },
+    "call":
+    {
+        "(": 1,
+        ")": 1
+    },
+    "math":
+    {
+        "+": 5,
+        "-": 5,
+        "*": 4,
+        "/": 4,
+        "%": 4,
+        "^": 3,
+        "^/": 2,
+        "|": 1
+    },
+    "comparison":
+    {
+        "=?": 1,
+        "!=": 1,
+        ">": 1,
+        "<": 1,
+        ">=": 1,
+        "<=": 1
+    },
+    "logic":
+    {
+        "not": 1,
+        "and": 2,
+        "or": 3,
+        "xor": 3
+    },
+    "equating":
+    {
+        "=": 1
+    },
+    "structure":
+    {
+        ":": 1
+    },
+    "key_word":
+    {
+        "while": 1,
+        "if": 1
+    },
+    "":
+    {
+        "": 1
+    }
 }
+
 
 class TokenError(Exception):
     pass
 
 
 class Token:
-    def __init__(self, name, group):
+    def __init__(self, name, group, sub_group, priority):
         self.name = name
         self.group = group
+        self.sub_group = sub_group
+        self.priority = priority
 
     def token_analyse(self):
         pass
 
     def __str__(self):
-        return f"{self.group} \"{self.name}\""
+        if self.sub_group == self.name:
+            return f"{self.group} \"{self.name}\" {self.priority}"
+        else:
+            return f"{self.sub_group} \"{self.name}\" {self.priority}"
 
     def __repr__(self):
-        return f"{self.group} \"{self.name}\""
+        return self.__str__()
 
 
 def token_split(text):
@@ -138,18 +211,21 @@ def command_split(tokens):
     add_command()
     return commands
 
+
 def token_analise(out_commands):
-    global commands
+    global commands, pre_group, priority
 
     result = []
     for command in commands:
         result += [[]]
         for token in command:
             group = ""
+            sub_group = ""
             if token.startswith(special_symbols["indent"]):
                 pass
             elif token.isdigit():
                 group = "object"
+                sub_group = "number"
             elif token in signs:
                 if token == "+":
                     group = "math"
@@ -164,6 +240,7 @@ def token_analise(out_commands):
                 elif token == "[":
                     if pre_group != "object":
                         group = "object"
+                        sub_group = "list"
                     else:
                         group = "sub_object"
                 elif token == "]":
@@ -181,9 +258,17 @@ def token_analise(out_commands):
                 group = "key_word"
             elif token[0].isalpha():
                 group = "object"
+                sub_group = "name"
             else:
                 raise TokenError(f"Неизвестный токен {token}")
-            token = Token(token, group)
+            if sub_group == empty:
+                sub_group = token
+            if not token.startswith(special_symbols["indent"]):
+                token_priority = [group_priority[group],
+                                  priority[group][sub_group]]
+            else:
+                token_priority = [group_priority[group], 1]
+            token = Token(token, group, sub_group, token_priority)
             result[-1] += [token]
             pre_group = group
     commands = result.copy()
@@ -192,18 +277,32 @@ def token_analise(out_commands):
 
 
 def command_analise():
-    pass
+    global commands
+
+    for command in commands:
+        expression_analise(command[1:])
 
 
-def expression_analise():
-    pass
+def expression_analise(expression):
+    if len(expression) == 1:
+        return expression
+    max_priority = max(expression,
+                       key=lambda token: token.priority[0]).priority[0]
+    more_priority = list(filter(lambda token: token.priority[0] == max_priority,
+                                expression))
+    max_priority = max(more_priority,
+                       key=lambda token: token.priority[1]).priority[1]
+    most_priority = list(filter(lambda token: token.priority[1] == max_priority,
+                                more_priority))
+    print(most_priority)
 
 
 if __name__ == '__main__':
     with open(f"{input()}.yotext", "r", encoding="utf-8") as infile:
         stage_1 = token_split(infile.read())
-    print(stage_1)
+    # print(stage_1)
     stage_2 = command_split(stage_1)
-    print(stage_2)
+    # print(stage_2)
     stage_3 = token_analise(stage_2)
-    print(stage_3)
+    # print(stage_3)
+    command_analise()
