3ace03d7f2daf59399d7d28a653e40b2f620695e
Gamba232 <parassitn@mail.ru>
2019-05-09 13:25:29 +0300

    09.05.2019

---
+++ YoVirMac/YoVirMac_0.5.py
@@ -0,0 +1,163 @@
+memory = []
+capacity = 32
+
+
+# процедуры для настройки ленты памяти
+def init_system_area():
+    pass
+
+
+def init_call_stack():
+    pass
+
+
+def init_memory_stack():
+    pass
+
+
+def init_memory_segment():
+    pass
+
+
+def load_program():
+    pass
+
+
+# низшие машинные команды
+def rdc():  # read cell
+    pass
+
+
+def wrc():  # write cell
+    pass
+
+
+# высшие машинные команды
+# переходы
+def end():  # end
+    pass
+
+
+def jmp():  # jump
+    pass
+
+
+def jif():  # jump if
+    pass
+
+
+# объекты
+def crt():  # create
+    pass
+
+
+def fnd():  # find
+    pass
+
+
+def eqt():  # equating
+    pass
+
+
+# ввод/вывод
+def inp():  # input
+    pass
+
+
+def out():  # output
+    pass
+
+
+# стек памяти
+def psh():  # push
+    pass
+
+
+def pop():  # pop
+    pass
+
+
+# вызовы
+def cal():  # call
+    pass
+
+
+def ret():  # return
+    pass
+
+
+# относительная адресация
+def raw():  # relative address write
+    pass
+
+
+def rar():  # relative address read
+    pass
+
+
+# логические операции
+def log_not():  # logical not
+    pass
+
+
+def log_and():  # logical and
+    pass
+
+
+def log_or():  # logical or
+    pass
+
+
+def log_xor():  # logical xor
+    pass
+
+
+# арифметические операции
+def neg():  # negative
+    pass
+
+
+def add():  # add
+    pass
+
+
+def inc():  # increment
+    pass
+
+
+def decr():  # decrement
+    pass
+
+
+def sub():  # subscription
+    pass
+
+
+def mul():  # multiplication
+    pass
+
+
+def div():  # division
+    pass
+
+
+def mod():  # module
+    pass
+
+
+# сравнение
+def eql():  # equal
+    pass
+
+
+def grt():  # greater
+    pass
+
+
+def les():  # less
+    pass
+
+
+# ничего
+def nop():  # no operation
+    pass
