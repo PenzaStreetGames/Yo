58db3a791a7431b5f590c0c3a9b5c8e13816b436
PenzaStreetGames <parassitn@mail.ru>
2019-06-09 21:19:55 +0300

    09.06.2019

--- yovirmac/main.py
+++ yovirmac/main.py
@@ -1,4 +1,4 @@
 from yovirmac.modules.constants import *
 from yovirmac.modules.lower_commands import *
 
-print(capacity)
+
--- yovirmac/modules/constants.py
+++ yovirmac/modules/constants.py
@@ -1,7 +1,7 @@
 from json import loads
 
-memory = []
 capacity = 32
+memory = []
 
 with open("config.yocfg", "r") as infile:
     config = loads(infile.read())
