cd4197078d2e3d10e99964404cad6984730d278b
Gamba232 <parassitn@mail.ru>
2019-06-10 12:05:36 +0300

    10.06.2019

--- yovirmac/modules/constants.py
+++ yovirmac/modules/constants.py
@@ -3,5 +3,12 @@ from json import loads
 memory = []
 capacity = 32
 
+
 with open("config.yocfg", "r") as infile:
     config = loads(infile.read())
+    types = config["types"]
+    types_length = config["types_length"]
+    seg_types = config["seg_types"]
+    seg_header = config["seg_header"]
+    seg_header_types = config["seg_header_types"]
+    segment_properties = config["segment_properties"]
