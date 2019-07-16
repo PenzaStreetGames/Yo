a9a624578efca7e34b07e66d6f203f4c7dc3e7c3
Gamba232 <parassitn@mail.ru>
2019-07-16 10:54:30 +0300

    16.07.2019 Теперь надо думать над средой разработки

---
+++ yocode/main.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtGui import QSyntaxHighlighter
+
+
+class Editor(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("yocode.ui", self)
+        highlighter = Highlighter(self.CodeArea.document())
+
+
+class Highlighter(QSyntaxHighlighter):
+
+    def __init__(self, document):
+        super().__init__(document)
+
+
+    def highlightBlock(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    editor = Editor()
+    editor.show()
+    sys.exit(app.exec_())
