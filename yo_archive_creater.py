d758ee22917d0f7c2c712518057b4584c79c41bb
chillay <47895140+chillay@users.noreply.github.com>
2019-08-06 19:20:38 +0300

    Update yo_archive_creater.py

--- yo_archive_creater.py
+++ yo_archive_creater.py
@@ -1,1 +1,9 @@
-
+import zipfile
+def pack(filename):
+    with zipfile.ZipFile(filename, 'w') as myzip:
+        myzip.write(f"{filename}.yotext")
+        myzip.write(f"{filename}.yovc")
+def open(filename):
+    z = zipfile.ZipFile(filename,'r')
+    z.extractall()
+    #надо:дать транслятору файл .yotext на съедение
