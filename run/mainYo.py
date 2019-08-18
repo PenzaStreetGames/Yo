24293c1d3861837c51f37a9012ff98d690160dda
chillay <47895140+chillay@users.noreply.github.com>
2019-08-18 13:38:27 +0300

    Create mainYo.py

---
+++ run/mainYo.py
@@ -0,0 +1,10 @@
+import sys
+from main import _translate
+from mainvm import run
+from yo_file import pack,_open
+import os
+path = sys.argv[1]
+pack(path)
+_open(path)
+_translate(path+".yotext")
+run(path+".yovc")
