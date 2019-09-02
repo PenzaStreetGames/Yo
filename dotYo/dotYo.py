8280e7575a5dbf375382785a574588c1452ed141
chillay <chapaev.ilya@yandex.ru>
2019-09-02 20:03:20 +0300

    commit message

--- dotYo/dotYo.py
+++ dotYo/dotYo.py
@@ -12,23 +12,58 @@ def pack(filename):
 
 
 def unpack(filename):
-    if "temp" not in os.listdir():
-        os.mkdir("temp")
+    print(os.listdir())
     z = zipfile.ZipFile(filename + ".yo", 'r')
-    os.chdir("temp")
-    try:
-        os.mkdir(filename)
-    except:
-        pass
-    os.chdir(filename)
-    z.extractall(path=os.getcwd())
+    # if filename + ".yotext" in os.listdir():
+    z.extract(filename + ".yotext")
+    # if filename + ".yovc" in os.listdir():
+    z.extract(filename + ".yovc")
     z.close()
 
 
-print(os.getcwd())
-path = input()
-compile_program(path, "en", mode="1")
-pack(path)
-unpack(path)
-print(path + ".yovc")
-run(path + ".yovc")
+def get_yotext(path):
+    # todo: добавить проверку на последнее редактирование файла .yotext
+    if f"{path}.yotext" in os.listdir():
+        compile_program(path, "en", mode="1")
+        pack(path)
+    if f"{path}.yo" not in os.listdir():
+        pack(path)
+    else:
+        unpack(path)
+    with open(path + ".yotext", encoding="utf-8") as f:
+        text = f.read()
+    os.remove(path + ".yotext")
+    return text
+
+
+def get_yovc(path):
+    # todo: добавить проверку на последнее редактирование файла .yotext
+    compile_program(path, "en", mode="1")
+    if f"{path}.yo" not in os.listdir():
+        pack(path)
+    else:
+        unpack(path)
+    with open(path + ".yovc", "rb") as f:
+        code = f.read()
+    os.remove(path + ".yovc")
+    return code
+
+
+def main():
+    # print(os.getcwd())
+    path = "program"  # input()
+    if f"{path}.yotext" in os.listdir():
+        compile_program(path, "en", mode="1")
+        pack(path)
+    if f"{path}.yo" not in os.listdir():
+        pack(path)
+    else:
+        unpack(path)
+    print(path + ".yovc")
+    run(path + ".yovc")
+    # os.remove(path+".yotext")
+    os.remove(path + ".yovc")
+
+
+if __name__ == '__main__':
+    print(get_yotext("program"))
---
+++ dotYo/test.py
@@ -0,0 +1,4 @@
+n = 100
+while n > 0:
+    print(n)
+    n = n - 1
